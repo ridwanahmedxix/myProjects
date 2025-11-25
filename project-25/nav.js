c142e08a2e894f04841f0a6a564454c77bd4aa07
ridwancodexr <ridwancodexr@gmail.com>
2025-11-25 10:21:08 +0600

    all update

---
+++ project-25/nav.js
@@ -0,0 +1,10 @@
+
+ function showSideBar() {
+    const showSideBar = document.querySelector(".sideBar")
+    showSideBar.style.display = "flex"
+ }
+
+ function hideSidebar() {
+    const showSideBar = document.querySelector(".sideBar")
+    showSideBar.style.display = "none"
+ }
