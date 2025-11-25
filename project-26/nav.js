ca504926eb59c3f632a5105ce1cb4dd41b1f44b7
ridwancodexr <ridwancodexr@gmail.com>
2025-11-25 10:01:31 +0600

    nav basic stractue create

---
+++ project-26/nav.js
@@ -0,0 +1,8 @@
+ function showSideBar() {
+    const showSideBar = document.querySelector(".sideBar") ;
+    showSideBar.style.display = "flex" ;
+ }
+ function hideBar() {
+    const showSideBar = document.querySelector(".sideBar") ;
+    showSideBar.style.display = "none" ;
+ }
