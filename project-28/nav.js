50bf7cb3656087bc1911cbec8a68e34fd8424644
ridwancodexr <ridwancodexr@gmail.com>
2025-11-27 09:39:02 +0600

    nav bar js code add

---
+++ project-28/nav.js
@@ -0,0 +1,13 @@
+
+
+  function showSidebar() {
+    const closeSidebar = document.querySelector(".sideBar") ;
+    closeSidebar.style.display = "flex" ;
+  }
+
+
+
+  function closeSidebar() {
+    const closeSidebar = document.querySelector(".sideBar") ;
+    closeSidebar.style.display = "none" ;
+  }
