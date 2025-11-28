1635be218421680f2fb4e7c1dda2068297ea6ee8
ridwancodexr <ridwancodexr@gmail.com>
2025-11-28 22:12:59 +0600

    all update

--- project-30/about.js
+++ project-30/about.js
@@ -3,3 +3,8 @@
     const hideSidebar = document.querySelector(".sideBar")
     hideSidebar.style.display = 'none'
  }
+ 
+ function showSideBar () {
+    const showSideBar = document.querySelector(".sideBar")
+    showSideBar.style.display = 'flex'
+ }
