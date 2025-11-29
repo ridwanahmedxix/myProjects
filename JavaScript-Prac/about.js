6b0ad82627d403471f6b9eeaa05e8497eb0aba02
ridwancodexr <ridwancodexr@gmail.com>
2025-11-29 20:38:53 +0600

    all select with js

---
+++ JavaScript-Prac/about.js
@@ -0,0 +1,4 @@
+const count = document.querySelector(".count") ;
+const btnMinus = document.querySelector(".btnMinus") ;
+const btnPlus = document.querySelector(".btnPlus") ;
+const reset = document.querySelector(".reset") ;
