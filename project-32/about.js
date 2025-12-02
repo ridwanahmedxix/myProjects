4557b28c3403cff75f4bac24b9c2ad45dee879c1
ridwancodexr <ridwancodexr@gmail.com>
2025-12-02 08:58:36 +0600

    responsive nav create complete

---
+++ project-32/about.js
@@ -0,0 +1,8 @@
+ function openSidebar() {
+    const openSidebar = document.querySelector(".sideBar")
+    openSidebar.style.display = 'flex'
+ }
+ function closeSidebar() {
+    const closeSidebar = document.querySelector(".sideBar")
+    closeSidebar.style.display = 'none'
+ }
