be56db41289675a2938e540c882174e9127b5736
ridwancodexr <ridwancodexr@gmail.com>
2025-12-03 19:01:28 +0600

    all update

--- project-34/about.js
+++ project-34/about.js
@@ -7,3 +7,9 @@ function closeSidebar() {
   const closeSidebar = document.querySelector(".sideBar");
   closeSidebar.style.display = "none";
 }
+
+document.querySelectorAll(".sideBar a").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.querySelector(".sideBar").style.display = "none";
+  });
+});
