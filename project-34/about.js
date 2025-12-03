53c827291c7c29a03b99b5fbae3b93b95d2970be
ridwancodexr <ridwancodexr@gmail.com>
2025-12-03 11:32:42 +0600

    nav create complete

---
+++ project-34/about.js
@@ -0,0 +1,9 @@
+function OpenSidebar() {
+  const OpenSidebar = document.querySelector(".sideBar");
+  OpenSidebar.style.display = "flex";
+}
+
+function closeSidebar() {
+  const closeSidebar = document.querySelector(".sideBar");
+  closeSidebar.style.display = "none";
+}
