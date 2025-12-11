43a8beea4d702a773b915fb6c9274c2a565caa83
ridwancodexr <ridwancodexr@gmail.com>
2025-12-11 13:35:07 +0600

    dynamic clock create done

--- JavaScriptProjects/practice/about.js
+++ JavaScriptProjects/practice/about.js
@@ -1,4 +1,10 @@
 function runningTime() {
   const now = new Date();
   let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  let timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").innerText = timeString;
 }
+setInterval(runningTime, 1000);
