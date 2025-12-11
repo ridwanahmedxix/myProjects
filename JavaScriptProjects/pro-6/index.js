6e943467db6a0d42cb15619ace24f2ea0068fc97
ridwancodexr <ridwancodexr@gmail.com>
2025-12-11 13:57:43 +0600

    all update

--- JavaScriptProjects/pro-6/index.js
+++ JavaScriptProjects/pro-6/index.js
@@ -1,8 +1,8 @@
 function runningTime() {
   const now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
+  let hours = now.getHours().toString().padStart(2, 0);
+  let minutes = now.getMinutes().toString().padStart(2, 0);
+  let seconds = now.getSeconds().toString().padStart(2, 0);
 
   let timeString = `${hours}:${minutes}:${seconds}`;
   document.getElementById("clock").innerText = timeString;
