87e014d00d238495dd9cc6b0d3681eb793b7d348
ridwancodexr <ridwancodexr@gmail.com>
2025-12-11 11:38:08 +0600

    js code create Live Clock

--- JavaScriptProjects/pro-6/index.js
+++ JavaScriptProjects/pro-6/index.js
@@ -1,10 +1,11 @@
 function runningTime() {
   const now = new Date();
-  let hours = now.getHours;
-  let minutes = now.getMinutes;
-  let seconds = now.getSeconds;
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
 
   let timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").innerText = timeString;
 }
 
-runningTime();
+setInterval(runningTime, 1000);
