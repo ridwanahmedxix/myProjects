a6251184cd1e6ec6250555d0cfda7bcbd3209355
ridwancodexr <ridwancodexr@gmail.com>
2025-12-12 14:50:46 +0600

    calculator create done

---
+++ JavaScriptProjects/practice/about.js
@@ -0,0 +1,16 @@
+const display = document.getElementById("display");
+
+function appendText(input) {
+  display.value = display.value + input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+function calculation() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
