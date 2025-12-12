bf7c0a8a168626636e957390f73ef016d1bd7f3e
ridwancodexr <ridwancodexr@gmail.com>
2025-12-12 08:07:35 +0600

    clock js code add

---
+++ JavaScriptProjects/practice/about.js
@@ -0,0 +1,6 @@
+function runningTime() {
+  const now = new Date();
+  let hours = now.getHours().toString().padStart();
+  let minutes = now.getMinutes().toString().padStart();
+  let seconds = now.getSeconds().toString().padStart();
+}
