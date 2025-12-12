e59ed8a022bfa75708366164fb0c757b8f6c61bb
ridwancodexr <ridwancodexr@gmail.com>
2025-12-12 08:10:23 +0600

    clock create done

--- JavaScriptProjects/practice/about.js
+++ JavaScriptProjects/practice/about.js
@@ -3,4 +3,8 @@ function runningTime() {
   let hours = now.getHours().toString().padStart();
   let minutes = now.getMinutes().toString().padStart();
   let seconds = now.getSeconds().toString().padStart();
+
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").innerText = timeString;
 }
+setInterval(runningTime, 1000);
