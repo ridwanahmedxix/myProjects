16f1df7a569b3a9a871b9e8273a80cc00ceac98e
ridwancodexr <ridwancodexr@gmail.com>
2025-12-15 06:42:20 +0600

    QR Code Generator Create Done

--- JavaScriptProjects/pro-8-Code-Generator/about.js
+++ JavaScriptProjects/pro-8-Code-Generator/about.js
@@ -4,4 +4,10 @@ const qrImg = document.getElementById("qr-img");
 
 qrBtn.addEventListener("click", () => {
   let inputValue = qrInput.value;
+
+  if (!inputValue) {
+    alert("Wrong Info");
+  } else {
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+  }
 });
