e4c65031e9643cf0e95138c9cf1890094e336683
ridwancodexr <ridwancodexr@gmail.com>
2025-12-15 06:29:10 +0600

    Input Value create

--- JavaScriptProjects/pro-8-Code-Generator/about.js
+++ JavaScriptProjects/pro-8-Code-Generator/about.js
@@ -1,3 +1,7 @@
 const qrInput = document.getElementById("qr-input");
 const qrBtn = document.getElementById("qr-btn");
 const qrImg = document.getElementById("qr-img");
+
+qrBtn.addEventListener("click", () => {
+  let inputValue = qrInput.value;
+});
