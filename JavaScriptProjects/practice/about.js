423e4bb208832d270590cfeb72320eb443889072
ridwancodexr <ridwancodexr@gmail.com>
2025-12-16 07:42:15 +0600

    all tag select with js

---
+++ JavaScriptProjects/practice/about.js
@@ -0,0 +1,4 @@
+const qrInput = document.getElementById("qrInput");
+const qrBtn = document.getElementById("qrBtn");
+const clearBtn = document.getElementById("clearBtn");
+const qrImg = document.getElementById("qrImg");
