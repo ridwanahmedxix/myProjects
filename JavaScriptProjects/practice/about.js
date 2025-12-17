9948b62697e38770c10f7a481ef3a339bf240fde
ridwancodexr <ridwancodexr@gmail.com>
2025-12-17 20:02:48 +0600

    clock html css code add done

---
+++ JavaScriptProjects/practice/about.js
@@ -0,0 +1,9 @@
+function runningTime() {
+  const now = new Date();
+  let hours = now.getUTCHours().toString().padStart(2, "0");
+  let minutes = now.getMinutes().toString().padStart(2, "0");
+  let seconds = now.getSeconds().toString().padStart(2, "0");
+
+  let timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").innerText = timeString;
+}
