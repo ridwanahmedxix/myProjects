681ff4622af8cfaac29349c672af4a75ad9fb0e8
ridwancodexr <ridwancodexr@gmail.com>
2025-12-20 18:25:38 +0600

    all clear

--- Practice/about.js
+++
@@ -1,8 +0,0 @@
-function openSideBar() {
-  const openSideBar = document.querySelector(".sideBar");
-  openSideBar.style.display = "flex";
-}
-function closeSideBar() {
-  const closeSideBar = document.querySelector(".sideBar");
-  closeSideBar.style.display = "none";
-}
