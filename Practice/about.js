9be41a0c10912b48820321436f8eee5876080658
ridwancodexr <ridwancodexr@gmail.com>
2025-12-20 18:38:52 +0600

    javascript code done - qr code gen

--- Practice/about.js
+++ Practice/about.js
@@ -2,3 +2,12 @@ const qrInput = document.getElementById("qrInput");
 const qrBtn = document.getElementById("qrBtn");
 const clearBtn = document.getElementById("clearBtn");
 const qrImg = document.getElementById("qrImg");
+
+qrBtn.addEventListener("click", () => {
+  let inputValue = qrInput.value;
+  if (!inputValue) {
+    alert("Wrong Information");
+  } else {
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+  }
+});
