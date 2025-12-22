d0d4456385519b4d3f6421e1d3193520b0072bb7
ridwancodexr <ridwancodexr@gmail.com>
2025-12-22 19:36:32 +0600

    js code add

--- JavaScriptProjects/pro-12-Drag-&-Drop-Or-BrowseFile/about.js
+++ JavaScriptProjects/pro-12-Drag-&-Drop-Or-BrowseFile/about.js
@@ -1,3 +1,29 @@
 const DragArea = document.querySelector(".appBody"),
   DragText = DragArea.querySelector("h3"),
-  button = DragArea.querySelector("button");
+  button = DragArea.querySelector("button"),
+  input = DragArea.querySelector("input");
+let MyFile;
+
+button.onclick = () => {
+  input.click();
+};
+
+// button.addEventListener("click", () => {
+//   input.click();
+// });
+
+input.addEventListener("change", function () {
+  MyFile = this.files[0];
+  DragArea.classList.add("active");
+});
+
+DragArea.addEventListener("dragover", (event) => {
+  event.preventDefault;
+  DragArea.classList.add("active");
+  DragText.textContent = "Release to Upload File";
+});
+
+DragArea.addEventListener("dragleave", () => {
+  DragArea.classList.remove("active");
+  DragText.textContent = "Drag & Drop";
+});
