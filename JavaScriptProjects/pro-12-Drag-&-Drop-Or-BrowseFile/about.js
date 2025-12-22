41f40eff1cae55d4e7fbd37ab067233d90a25f3c
ridwancodexr <ridwancodexr@gmail.com>
2025-12-22 20:03:51 +0600

    Drag & Drop Or Browse File Upload Create Done

--- JavaScriptProjects/pro-12-Drag-&-Drop-Or-BrowseFile/about.js
+++ JavaScriptProjects/pro-12-Drag-&-Drop-Or-BrowseFile/about.js
@@ -15,10 +15,12 @@ button.onclick = () => {
 input.addEventListener("change", function () {
   MyFile = this.files[0];
   DragArea.classList.add("active");
+
+  showMe();
 });
 
 DragArea.addEventListener("dragover", (event) => {
-  event.preventDefault;
+  event.preventDefault();
   DragArea.classList.add("active");
   DragText.textContent = "Release to Upload File";
 });
@@ -27,3 +29,29 @@ DragArea.addEventListener("dragleave", () => {
   DragArea.classList.remove("active");
   DragText.textContent = "Drag & Drop";
 });
+
+DragArea.addEventListener("drop", (event) => {
+  event.preventDefault();
+  MyFile = event.dataTransfer.files[0];
+
+  showMe();
+});
+
+function showMe() {
+  let fileType = MyFile.type;
+  let validEx = ["image/jpeg", "image/jpg", "image/png"];
+
+  if (validEx.includes(fileType)) {
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+      let imgUrl = fileReader.result;
+      let img = `    <img src="${imgUrl}" alt="">`;
+      DragArea.innerHTML = img;
+    };
+    fileReader.readAsDataURL(MyFile);
+  } else {
+    alert("This file is not vaild");
+    DragArea.classList.remove("active");
+    DragText.textContent = "Drag & Drop";
+  }
+}
