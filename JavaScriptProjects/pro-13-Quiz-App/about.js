da40e9c71750e71e7517b91aa5acc570f39041c6
ridwancodexr <ridwancodexr@gmail.com>
2025-12-24 16:01:39 +0600

    some bug fixed

--- JavaScriptProjects/pro-13-Quiz-App/about.js
+++ JavaScriptProjects/pro-13-Quiz-App/about.js
@@ -15,11 +15,22 @@ exitBtn.onclick = () => {
 contiBtn.onclick = () => {
   rulesBox.classList.remove("activeInfo");
   questions.classList.add("activeQuiz");
-  showQuestions();
+  showQuestions(0);
 };
 
-function showQuestions() {
+function showQuestions(index) {
   const que_text = document.querySelector(".ParentTextWDH");
-  let que_tag = "<span>" + questionsList[0].question + "</span>";
+  const option_list = document.querySelector(".myOptions");
+  let option_tag =
+    "<div class='options'>" + questionsList[index].options + "</div>";
+
+  let que_tag =
+    "<span>" +
+    questionsList[index].numb +
+    "." +
+    questionsList[index].question +
+    "</span>";
   que_text.innerHTML = que_tag;
+
+  option_list.innerHTML = option_tag;
 }
