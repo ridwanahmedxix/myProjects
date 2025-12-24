49f0776cf0c886fcb8f5c6da75fd8b8de591baaf
ridwancodexr <ridwancodexr@gmail.com>
2025-12-24 12:24:25 +0600

    some update with js

--- JavaScriptProjects/pro-13-Quiz-App/about.js
+++ JavaScriptProjects/pro-13-Quiz-App/about.js
@@ -1,6 +1,8 @@
 const myBtn = document.querySelector(".myBtn button ");
 const rulesBox = document.querySelector(".rulesBox");
 const exitBtn = document.querySelector(".exitBtn");
+const questions = document.querySelector(".questions");
+const contiBtn = document.querySelector(".contiBtn");
 
 myBtn.onclick = () => {
   rulesBox.classList.add("activeInfo");
@@ -8,4 +10,8 @@ myBtn.onclick = () => {
 
 exitBtn.onclick = () => {
   rulesBox.classList.remove("activeInfo");
+};
+
+contiBtn.onclick = () => {
+  questions.classList.add("activeInfo");
 };
