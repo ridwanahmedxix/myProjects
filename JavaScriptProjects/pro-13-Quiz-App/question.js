4aa107959c43a88dc87d9860a65ce31b3be0780b
ridwancodexr <ridwancodexr@gmail.com>
2025-12-24 15:08:46 +0600

    question data create

---
+++ JavaScriptProjects/pro-13-Quiz-App/question.js
@@ -0,0 +1,43 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What Does HTML Stand For",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "  Hyper Text Preprocessor ",
+      " Hyper Text Markup Language ",
+      " Hyper Text Multiple Language ",
+      " Hyper Tool Multi Language ",
+    ],
+  },
+
+  {
+    numb: 2,
+    question: "What Does CSS Stand For",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Creative Style System",
+      "Cascading Style Sheets",
+      "Computer Style Sheets",
+      "Colorful Style Sheets",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What Does JS Stand For",
+    answer: "JavaScript",
+    options: ["Java Source", "Just Script", "JavaScript", "JSON Script"],
+  },
+  {
+    numb: 4,
+    question: "Which HTML tag is used to create a hyperlink",
+    answer: "<a>",
+    options: ["<link>", "<a>", "<href>", "<url>"],
+  },
+  {
+    numb: 5,
+    question: "Which CSS property is used to change text color",
+    answer: "color",
+    options: ["font-color", "text-color", "color", "background-color"],
+  },
+];
