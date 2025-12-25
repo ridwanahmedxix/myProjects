f7b93f42e0210f00b2321ac9ede9c7872ccdfba9
ridwancodexr <ridwancodexr@gmail.com>
2025-12-25 19:29:41 +0600

    all update

--- JsMiniPro/pro1_Click_Message/script.js
+++ JsMiniPro/pro1_Click_Message/script.js
@@ -1,6 +1,9 @@
 const Display = document.getElementById("Display");
 const click_Me = document.getElementById("click_Me");
+const parentDiv = document.querySelector(".parentDiv");
 
 click_Me.addEventListener("click", () => {
   Display.innerText = " I LOVE YOU ";
+  Display.style.display = "block";
+  parentDiv.style.display = "block";
 });
