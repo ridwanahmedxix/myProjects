33287d6afc331a3ec4dfaa28ef1f9802180abb5d
ridwancodexr <ridwancodexr@gmail.com>
2025-12-25 19:15:57 +0600

    Click Message html and js code done

---
+++ JsMiniPro/pro1_Click_Message/script.js
@@ -0,0 +1,6 @@
+const Display = document.getElementById("Display");
+const click_Me = document.getElementById("click_Me");
+
+click_Me.addEventListener("click", () => {
+  Display.innerText = " I LOVE YOU ";
+});
