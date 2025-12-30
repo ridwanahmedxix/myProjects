c6d559b229225f0214a6023d6f5e5aaf6e82d3bd
ridwancodexr <ridwancodexr@gmail.com>
2025-12-30 14:22:55 +0600

    Click Message

--- JavaScriptProjects/practice/about.js
+++ JavaScriptProjects/practice/about.js
@@ -1,3 +1,10 @@
 const display = document.getElementById("display");
 const UserInput = document.getElementById("UserInput");
 const clickMe = document.getElementById("clickMe");
+
+clickMe.addEventListener("click", () => {
+  let inputValue = UserInput.value;
+  display.textContent = inputValue;
+  UserInput.value = "";
+  clearBtn.style.display = "block";
+});
