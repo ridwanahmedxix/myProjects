e9738519135678741b4bae39e679401bd29661b7
ridwancodexr <ridwancodexr@gmail.com>
2025-12-30 09:10:25 +0600

    all tag selecte

---
+++ JavaScriptProjects/pro-14-Text-Highlight/script.js
@@ -0,0 +1,6 @@
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const Case_box = document.getElementById("Case_box");
+const count = document.getElementById("count");
+const textBox = document.getElementById("textBox");
+const notFound = document.getElementById("notFound");
