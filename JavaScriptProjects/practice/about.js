b7e2223b54e2ff42016b2d19ae4928bed94c2b54
ridwancodexr <ridwancodexr@gmail.com>
2025-12-31 09:42:26 +0600

    Todo Add Function create

--- JavaScriptProjects/practice/about.js
+++ JavaScriptProjects/practice/about.js
@@ -1,3 +1,11 @@
 const display = document.getElementById("display");
 const userInput = document.getElementById("userInput");
 const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", () => {
+  let inputValue = userInput.value;
+  userInput.value = " ";
+  const li = document.createElement("li");
+  li.textContent = inputValue;
+  display.append(li);
+});
