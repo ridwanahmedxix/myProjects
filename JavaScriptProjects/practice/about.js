13d72ae3f6912cc46be9aeab03759bfcf76aff94
ridwancodexr <ridwancodexr@gmail.com>
2025-12-31 14:15:35 +0600

    Todo Add Function create revision - 2

---
+++ JavaScriptProjects/practice/about.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+const userInput = document.getElementById("userInput");
+const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", () => {
+  let inputValue = userInput.value.trim();
+  userInput.value = "";
+  const li = document.createElement("li");
+  li.textContent = inputValue + "";
+  display.append(li);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+  li.append(deleteBtn);
+
+  if (inputValue === "") {
+    alert("Wrong");
+  }
+});
