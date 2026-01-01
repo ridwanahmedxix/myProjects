4c5bf739e119e007f3edf99b16ae94fe670b9573
ridwancodexr <ridwancodexr@gmail.com>
2026-01-01 09:15:04 +0600

    alert create

--- JavaScriptProjects/practice/about.js
+++ JavaScriptProjects/practice/about.js
@@ -5,7 +5,10 @@ const addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", () => {
   let inputValue = userInput.value.trim();
   userInput.value = "";
-
+  if (inputValue === "") {
+    alert("Something Wrong");
+    return;
+  }
   const li = document.createElement("li");
   li.textContent = inputValue;
   display.append(li);
