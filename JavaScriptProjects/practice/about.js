290c217bba64b214f0abd6a3df78dced45dba642
ridwancodexr <ridwancodexr@gmail.com>
2026-01-01 09:08:47 +0600

    Anything Add System create

---
+++ JavaScriptProjects/practice/about.js
@@ -0,0 +1,12 @@
+const display = document.getElementById("display");
+const userInput = document.getElementById("userInput");
+const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", () => {
+  let inputValue = userInput.value.trim();
+  userInput.value = "";
+
+  const li = document.createElement("li");
+  li.textContent = inputValue;
+  display.append(li);
+});
