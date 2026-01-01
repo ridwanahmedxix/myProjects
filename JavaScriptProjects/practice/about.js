0f2b5c0187305cacb03f0f884e273ac98a1fbb88
ridwancodexr <ridwancodexr@gmail.com>
2026-01-01 11:22:54 +0600

    vowel counter revision 1

--- JavaScriptProjects/practice/about.js
+++ JavaScriptProjects/practice/about.js
@@ -1,3 +1,27 @@
 const textArea = document.getElementById("textArea");
 const countBtn = document.getElementById("countBtn");
 const outPut = document.getElementById("outPut");
+
+countBtn.addEventListener("click", () => {
+  if (textArea.value === "") {
+    outPut.textContent = " Please write something ";
+    outPut.style.color = "red";
+  } else {
+    let areaValue = textArea.value;
+    let count = 0;
+
+    for (let i = 0; i < areaValue.length; i++) {
+      if (
+        areaValue[i] === "a" ||
+        areaValue[i] === "e" ||
+        areaValue[i] === "i" ||
+        areaValue[i] === "o" ||
+        areaValue[i] === "u"
+      ) {
+        count++;
+      }
+    }
+
+    outPut.textContent = `The number of vowel ${count}`;
+  }
+});
