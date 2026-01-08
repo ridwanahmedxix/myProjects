3d978a5a2c4d12a3322ef901724f9a093d0a20de
ridwancodexr <ridwancodexr@gmail.com>
2026-01-08 19:27:33 +0600

    Responsive Nav Create

---
+++ project-36/about.js
@@ -0,0 +1,8 @@
+function ShowMenubar() {
+  const ShowMenubar = document.querySelector(".sideBar");
+  ShowMenubar.style.display = "flex";
+}
+function closeSideBar() {
+  const closeSideBar = document.querySelector(".sideBar");
+  closeSideBar.style.display = "none";
+}
