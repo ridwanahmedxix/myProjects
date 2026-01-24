ab5a6fd3b2ac654b48fe838014ebea806fdfa0a4
ridwancodexr <ridwancodexr@gmail.com>
2026-01-24 11:27:17 +0600

    Navbar Create

--- project-41/index.js
+++ project-41/index.js
@@ -1,6 +1,10 @@
 const closeSidebar = document.getElementById("closeSidebar");
+const openSideBar = document.getElementById("openSideBar");
 const sideBar = document.querySelector(".sideBar");
 
 closeSidebar.addEventListener("click", () => {
   sideBar.style.display = "none";
+});
+openSideBar.addEventListener("click", () => {
+  sideBar.style.display = "flex";
 });
