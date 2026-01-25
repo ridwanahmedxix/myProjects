bb37a63a84592689fa6b5932774226509ce7b6c9
ridwancodexr <ridwancodexr@gmail.com>
2026-01-25 19:05:40 +0600

    all update

--- JavaScriptProjects/practice/about.js
+++ JavaScriptProjects/practice/about.js
@@ -1,13 +1,25 @@
-const display = document.getElementById("display");
-const userInpu = document.getElementById("userInpu");
-const addBtn = document.getElementById("addBtn");
+// To access the show button element
+let showbtn = document.getElementById("showbtn");
 
-addBtn.addEventListener("click", () => {
-  let inputValue = userInpu.ariaValueMax.trim();
-  userInpu.value = "";
+// To access the Close button element
+let closebtn = document.getElementById("closebtn");
 
-  const li = document.createElement("li");
+// To acces the popup element
+let popup = document.querySelector(".popup");
+let subp = document.getElementById("sub-p");
 
-  li.textContent = inputValue;
-  display.append(li);
+// To show the popup on click
+showbtn.addEventListener("click", () => {
+  popup.style.display = "block";
+  showbtn.style.display = "none";
+  document.body.style.backgroundColor = "#9EA9B1";
+  subp.style.display = "none";
+});
+
+// To close the popup on click
+closebtn.addEventListener("click", () => {
+  popup.style.display = "none";
+  showbtn.style.display = "block";
+  document.body.style.backgroundColor = "#09dbd450";
+  subp.style.display = "block";
 });
