ed489aec5df0a3bf77616760eef92ad0af79ed14
ridwancodexr <ridwancodexr@gmail.com>
2026-01-29 08:41:06 +0600

    Click To Popup show

---
+++ JsMiniPro/pro-3/script.js
@@ -0,0 +1,6 @@
+const clickMe = document.getElementById("clickMe");
+const popUp = document.getElementById("popUp");
+
+clickMe.addEventListener("click", () => {
+  popUp.style.display = "block";
+});
