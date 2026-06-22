7b88677d0cfd41276f5baa51adf7e2729f10e756
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-22 21:01:25 +0600

    nav daynamic

---
+++ project-57/script.js
@@ -0,0 +1,8 @@
+function closeBar() {
+  const closeBar = document.querySelector(".sideBar");
+  closeBar.style.display = "none";
+}
+function openBar() {
+  const openBar = document.querySelector(".sideBar");
+  openBar.style.display = "flex";
+}
