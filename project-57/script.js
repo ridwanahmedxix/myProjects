995a92c0c3916632f4164f40037c8cbba42b8c6d
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-22 16:30:01 +0600

    responsive nav bar create done

---
+++ project-57/script.js
@@ -0,0 +1,8 @@
+function openBar() {
+  const openBar = document.querySelector(".sideBar");
+  openBar.style.display = "flex";
+}
+function closeBar() {
+  const closeBar = document.querySelector(".sideBar");
+  closeBar.style.display = "none";
+}
